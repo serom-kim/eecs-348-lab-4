4a9cdfee8fbe3110cf72a91b4fa5f265d77fb991
Serom <serom.kim@ku.edu>
2024-10-04 16:46:24 -0500

    running

--- code1.c
+++ code1.c
@@ -5,11 +5,16 @@ void combinations(int score) {
 
 //series of nested loops to go through possible TDs, FGs, and safeties
     for (int td2 = 0; td2 * 8 <= score; td2++) {
+        
         for (int td1 = 0; td1 * 7 + td2 * 8 <= score; td1++) {
+            
             for (int td = 0; td * 6 + td1 * 7 + td2 * 8 <= score; td++) {
+                
                 for (int fg = 0; fg * 3 + td * 6 + td1 * 7 + td2 * 8 <= score; fg++) {
+                    
                     for (int safety = 0; safety * 2 + fg * 3 + td * 6 + td1 * 7 + td2 * 8 <= score; safety++) {
                         int total = td2 * 8 + td1 * 7 + td * 6 + fg * 3 + safety * 2;
+                        
                         if (total == score) {
                             printf("%d TD + 2pt, %d TD + FG, %d TD, %d FG, %d Safety\n", td2, td1, td, fg, safety);
                         }
